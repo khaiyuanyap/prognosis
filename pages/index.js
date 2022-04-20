4e8023bc04283b58df193b39530c58a97ba7a96b
YuanYap <khaiyuanyap@gmail.com>
2022-04-20 22:47:45 +0800

    work in progress

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function home() {
         <div className="inset-0 items-center justify-center md:absolute md:flex">
           <div className="grid gap-4 grid-cols-3 justify-items-center">
             <Link href="/cardiac" passHref>
-              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
+              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border-2 p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
                 <div>
                   <img src="/cardiac.png" className="contain" />
                 </div>
@@ -25,7 +25,7 @@ export default function home() {
               </a>
             </Link>
             <Link href="/diabetes" passHref>
-              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
+              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border-2 p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
                 <div>
                   <img src="/diabetes.png" className="contain" />
                 </div>
@@ -35,7 +35,7 @@ export default function home() {
               </a>
             </Link>
             <Link href="/melanoma" passHref>
-              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
+              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border-2 p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
                 <div>
                   <img src="/skins.png" className="contain" />
                 </div>
@@ -45,12 +45,32 @@ export default function home() {
               </a>
             </Link>
             <Link href="/hypertension" passHref>
-              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
+              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border-2 p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
                 <div>
                   <img src="/hyper.png" className="contain" />
                 </div>
                 <p className="mt-4 text-center text-xs font-semibold">
                   Hypertension prediction
+                </p>
+              </a>
+            </Link>
+            <Link href="/hypertension" passHref>
+              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border-2 p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
+                <div>
+                  <img src="/work-in-progress-hi.png" className="contain" />
+                </div>
+                <p className="mt-4 text-center text-xs font-semibold">
+                  Mental health
+                </p>
+              </a>
+            </Link>
+            <Link href="/hypertension" passHref>
+              <a className="my-4 h-20 w-20 cursor-pointer rounded-lg border-2 p-2 duration-200 hover:border-red-400 hover:bg-red-50 md:h-32 md:w-32">
+                <div>
+                  <img src="/work-in-progress-hi.png" className="contain" />
+                </div>
+                <p className="mt-4 text-center text-xs font-semibold">
+                  About us
                 </p>
               </a>
             </Link>
