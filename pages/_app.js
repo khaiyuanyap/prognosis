181daffc5336cf33b0dbcd87b0769db142dd40c3
YuanYap <khaiyuanyap@gmail.com>
2022-04-24 15:10:54 +0800

    manifest.json

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import "../styles/globals.css"
+import Head from "next/head"
 
 function MyApp({Component, pageProps}) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+				<meta
+					name="viewport"
+					content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+				/>
+				<meta name="description" content="Description" />
+				<meta name="keywords" content="Keywords" />
+				<link rel="manifest" href="/manifest.json" />
+				<meta name="theme-color" content="#f9fafb" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
 
 export default MyApp
